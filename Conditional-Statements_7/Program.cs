3b59e8101b81612f53bf4b6200520c1d9617d3c3
Nikita-284 <30615329+Nikita-284@users.noreply.github.com>
2024-02-27 13:47:28 -0500

    practice conditional and loops

--- Conditional-Statements_7/Program.cs
+++ Conditional-Statements_7/Program.cs
@@ -6,147 +6,54 @@ conditional statements after getting input from users
 */
 
 using System;
-// Ask the user for input
-Console.WriteLine("Please enter a Sales Amount ...");
-/*
-    Use the try catch block to validate user input.
-    If the user provides bad input, the catch block
-    will handle the error and a message will be displayed.
-*/
-try
+using System.Diagnostics;
+
+class Program
 {
-    // This variable will gather the user input
-    string input = Console.ReadLine();
-    /* 
-        This variable will be used to store sales
-        after the input variable is parsed to decimal
-        */
-    decimal sales = decimal.Parse(input);
-    // This variable will be used to calculate the bonus
-    decimal bonus = 0;
-    // This variable will be used to get the number of years user has been in sales
-    int years_in_sales = 0;
-    /* 
-        This IF/ELSE IF/ELSE statement is used to conditionally
-        test the users input. Various options are available
-        depending on the amount entered by the user. Then, insde of each IF/ELSE IF/ELSE
-        there is a switch conditional statement that displays a message depending
-        on the number of years experience in sales entered by the user. 
-        */
-    if (sales < 1000)
+    static void Main(string[] args)
     {
-        bonus += .05m * sales;
-        Console.WriteLine("Your 5% bonus for the Sales Amount " + String.Format("{0:C}", sales) + " is: " + String.Format("{0:C}", bonus));
-        // Now get the number of years the user has been in sales
-        Console.WriteLine("How many years have you been in Sales?");
-        // Assign the number of years the user has input to the years_in_sales variable
-        years_in_sales = int.Parse(Console.ReadLine());
-        /* Execute the conditional switch statement to output different messages 
-            depending on the integer value entered for years in sales
-        */
-        switch (years_in_sales)
+        decimal income = 10000.45M;
+        decimal taxRate = 0.1M;
+        decimal taxLiability = income * taxRate;
+        Debug.WriteLine("Tax liability is " + taxLiability);
+
+        //if statement
+        if (income < 400000)
         {
-            case 0:
-                Console.WriteLine("Wow! Pretty good for a rookie!");
-                Console.WriteLine("Press any key to exit the program ...");
-                // Console.ReadKey(true) pauses the program until the user presses a key
-                Console.ReadKey(true);
-                break;
-            case 1:
-                Console.WriteLine("Not bad for one year of sales experience!");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-            case 2:
-                Console.WriteLine("Not bad for two years of sales experience!");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-            default:
-                Console.WriteLine("You have many years of sales experience. Keep up the good work!");
-                Console.WriteLine("However, if are here because you did not type an integer value for the number of years - Please enter an integer value next time ...");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-        } // end of switch              
-    }
-    else if ((sales > 1000) && (sales < 3000))
-    {
-        bonus += .06m * sales;
-        Console.WriteLine("Your 6% bonus for the Sales Amount " + String.Format("{0:C}", sales) + " is: " + String.Format("{0:C}", bonus));
-        Console.WriteLine("How many years have you been in Sales?");
-        // Assign the number of years the user has input to the years_in_sales variable
-        years_in_sales = int.Parse(Console.ReadLine());
-        /* Execute the conditional switch statement to output different messages 
-            depending on the integer value entered for years in sales
-        */
-        switch (years_in_sales)
+            taxLiability = 0;
+        }
+        else
         {
-            case 0:
-                Console.WriteLine("Wow! Pretty good for a rookie!");
-                Console.WriteLine("Press any key to exit the program ...");
-                // Console.ReadKey(true) pauses the program until the user presses a key
-                Console.ReadKey(true);
-                break;
-            case 1:
-                Console.WriteLine("Not bad for one year of sales experience!");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-            case 2:
-                Console.WriteLine("Not bad for two years of sales experience!");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-            default:
-                Console.WriteLine("You have many years of sales experience. Keep up the good work!");
-                Console.WriteLine("However, if are here because you did not type an integer value for the number of years - Please enter an integer value next time ...");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-        } // end of switch 
-    }
-    else
-    {
-        bonus += .07m * sales;
-        Console.WriteLine("Your 7% bonus for the Sales Amount " + String.Format("{0:C}", sales) + " is: " + String.Format("{0:C}", bonus));
-        Console.WriteLine("How many years have you been in Sales?");
-        // Assign the number of years the user has input to the years_in_sales variable
-        years_in_sales = int.Parse(Console.ReadLine());
-        /* Execute the conditional switch statement to output different messages 
-            depending on the integer value entered for years in sales
-        */
-        switch (years_in_sales)
+            taxRate = 0.5M;
+            taxLiability = taxRate * income;
+        }
+        Debug.WriteLine("Tax liability is " + taxLiability);
+
+        //while loop
+        //area of triangle
+        double area = 0;
+
+        bool stopProgram = false;
+        while (stopProgram == false)
         {
-            case 0:
-                Console.WriteLine("Wow! Pretty good for a rookie!");
-                Console.WriteLine("Press any key to exit the program ...");
-                // Console.ReadKey(true) pauses the program until the user presses a key
-                Console.ReadKey(true);
-                break;
-            case 1:
-                Console.WriteLine("Not bad for one year of sales experience!");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-            case 2:
-                Console.WriteLine("Not bad for two years of sales experience!");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-            default:
-                Console.WriteLine("You have many years of sales experience. Keep up the good work!");
-                Console.WriteLine("Press any key to exit the program ...");
-                Console.ReadKey(true);
-                break;
-        } // end of switch 
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 24;
+            int c = 25;
+
+            float s = (a + b + c) / 2;
+
+            if (a > 0)
+            {
+                area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            }
+            
+            else
+            {
+                stopProgram = true;
+            }
+            Console.WriteLine("Area of triangle is " +  area);
+        } // end while loop
+        
+
     }
-} // end of try
-catch
-{
-    Console.WriteLine("Please use a decimal data type for your sales amount next time...");
-    Console.WriteLine("--- OR ---");
-    Console.WriteLine("Please use an integer data type for your number of years in sales next time ...");
-    Console.WriteLine("Press any key to exit the program and try again...");
-    Console.ReadKey(true);
-} // end of catch     
+}
